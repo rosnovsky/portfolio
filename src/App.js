fb55ade6a4ccd5a503cc0f3b5c94d907702e74e3
Artem Rosnovskiy <inbox@podtema.com>
2016-12-13 15:07:46 -0800

    Minor update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,69 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+<div>
+  <div className="row">
+    <nav>
+      <ul>
+        <li><a href="https://github.com/rosnovsky" target="_blank">github</a></li>
+        <li><a href="https://linkedin.com/in/rosnovsky" target="_blank">linkedin</a></li>
+        <li><a href="https://facebook.com/rosnovsky" target="_blank">facebook</a></li>
+        <li><a href="https://twitter.com/rosnovsky" target="_blank">twitter</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <div className="row title">
+    <h1>Artem Rosnovskiy</h1>
+    <h3>Front-End Developer</h3>
+  </div>
+
+  <div className="row content">
+    <div className="six columns">
+      <h4>Hi there!</h4>
+      <p>Thanks for dropping by. I design and develop easy-to-use and clear user interfaces for web sites and applications, both web and iOS/Android. I fully embrace mobile-first, device-specific designs, latest standards and best practices in producing and developing interfaces and websites. </p>
+      <p><a href="artem-rosnovskiy-front-end.pdf">Download my updated resumé</a> and drop me a line at <a href="mailto:artem.rosnovsky@gmail.com" className="email">artem.rosnovsky@gmail.com</a>. I’m <span className="unavailable">not available</span> for hire.</p>
+    </div>
+    
+    <div className="six columns">
+      <h4>Skills &amp; Expertise</h4>
+      <p><strong>Web dev</strong>: HTML5, CSS3, Javascript, jQuery, Gulp, Webpack, npm, Sass, Bootstrap, git, Ubuntu, Apache, nginx, PHP, MySQL</p>
+      <p><strong>Design and Media</strong>: Adobe Photoshop, Illustrator, AfterEffectr, Premier Pro, Dreamweaver, Flash (including ActionScript), Audition, Sketch by Bohemian</p>
+      <p><strong>In Progress</strong>: React, Node.js, Angular 2, MongoDB, Swift</p>
+    </div>
+  </div>
+
+  <div className="row">
+    <div className="twelve columns">
+      <h4>Current Reading</h4>
+    </div>
+  </div>
+
+  <div className="row books">
+    <div className="three columns book">
+      <a href="http://amzn.to/2epRd6H" target="_blank"><img src="images/22814239.jpg" alt="You don&#39;t know JS"/>
+      <p>You Don&#39;t Know JS: this &amp; Object Prototypes</p></a>
+    </div>
+    
+    <div className="three columns book">
+      <a href="http://amzn.to/2eSLIxI" target="_blank"><img src="images/20830437.jpg" alt="CSS Secrets"/>
+      <p>CSS Secrets: Better Solutions to Everyday Web Design Problems</p></a>
+    </div>
+    <div className="three columns book">
+      <a href="http://amzn.to/2eSLN4u" target="_blank"><img src="images/17204679.jpg" alt="Man&#39;s Search for Meaning"/>
+      <p>Man&#39;s Search for Meaning</p></a>
+    </div>
+    <div className="three columns book">
+      <a href="http://amzn.to/2flyqOx" target="_blank"><img src="images/27426984.jpg" alt="Disrupted"/>
+      <p>Disrupted: My Misadventure in the Start-Up Bubble</p></a>
+    </div>
+  </div>
+
+  <div className="row footer">
+    <p>© 2017</p>
+  </div> 
+</div>
+      );
   }
 }
 
