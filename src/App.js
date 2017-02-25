0e5aaadea309e492375c1651d0e87db7f67bcc1b
Artem Rosnovskiy <inbox@podtema.com>
2017-02-25 09:20:45 -0800

    typos

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ class App extends Component {
     
     <div className="six columns">
       <h4>Skills &amp; Expertise</h4>
-      <p><strong>Web dev</strong>: HTML5, CSS3, Javascript, jQuery, Gulp, Webpack, npm, Sass, Bootstrap, git, Ubuntu, Apache, nginx, PHP, MySQL</p>
-      <p><strong>Design and Media</strong>: Adobe Photoshop, Illustrator, AfterEffectr, Premier Pro, Dreamweaver, Flash (including ActionScript), Audition, Sketch by Bohemian, Adobe Experience Manager (AEM)</p>
+      <p><strong>Web dev</strong>: HTML5, CSS3, Javascript, jQuery, Gulp, Webpack, npm, Sass, Bootstrap, git, Ubuntu, Apache, nginx, PHP, MySQL, SSL</p>
+      <p><strong>Design and Media</strong>: Adobe Photoshop, Illustrator, AfterEffects, Premier Pro, Dreamweaver, Flash (including some ActionScript), Audition, Sketch by Bohemian, Adobe Experience Manager (AEM)</p>
       <p><strong>In Progress</strong>: ES6, React, Node.js, Angular 2, MongoDB, Swift, WebOps</p>
     </div>
   </div>
