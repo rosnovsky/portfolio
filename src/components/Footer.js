2fc3f5b526c65b9c9ea67bd396cdfed31fc6a9ea
Artem Rosnovskiy <inbox@podtema.com>
2017-05-15 11:06:43 -0700

    Cleanup and initial reset

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = function Footer(props) {
-  return <div className="row footer"><p>Copyright © <span className="currentYear">{props.currentYear}</span> Artem Rosnovskiy.<br/> Online since 1996.</p></div>;
-}
-
-export default Footer;
