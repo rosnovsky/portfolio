784474230ba1b2e0ea8b6caebde41d17fedc8bf0
Artem Rosnovskiy <inbox@podtema.com>
2017-05-14 17:42:10 -0700

    Status update

---
+++ public/scripts/apiKeys.js
@@ -0,0 +1,6 @@
+export function apiKeys() {
+	const foursqApiKey = "YBMN154PRDHFF01BVVIG4UN3ONTXQQG3VK30IKR3BHXAFTNO";
+	const wakatimeKey = "d7ecb2e7-fc89-4faf-a778-874a5742e251";
+	const fitbitToken = "access_token=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyMjZQUzUiLCJhdWQiOiIyMjhITEIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyaHIiLCJleHAiOjE1MjYzMjcxMTgsImlhdCI6MTQ5NDc5MTExOH0.gAjeW0iXfX_89rdxtOtIs3QcKKdH8ODkc5eGxvlUQQY&user_id=226PS5&scope=heartrate&token_type=Bearer&expires_in=31536000"
+	return foursqApiKey, wakatimeKey, fitbitToken;
+}
