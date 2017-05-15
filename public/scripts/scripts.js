10c02ac1d30caeb14e21336173a729efa0386bc5
Artem Rosnovskiy <inbox@podtema.com>
2017-05-14 20:21:14 -0700

    Obsufscating and minifying stuff

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -59,13 +59,8 @@ const getBook = async (bookID) => {
 
 const getCodingTime = async () => {
 	//Wakatime API stats request
-	const opts = {
-		  	method: 'GET',
-			header: { 	'Origin' : 'http://rosnovsky.us',
-						'Access-Control-Allow-Origin' : 'http://rosnovsky.us' }
-		};
 
-	const url = 'https://crossorigin.me/https://wakatime.com/api/v1/users/rosnovsky/stats/last_7_days?api_key=d7ecb2e7-fc89-4faf-a778-874a5742e251';
+	const url = 'https://wakatime.com/api/v1/users/rosnovsky/stats/last_7_days?api_key=d7ecb2e7-fc89-4faf-a778-874a5742e251';
 	
 	//fetching the data
 	const result = await fetch(url);
