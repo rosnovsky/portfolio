55148b952e5486b68228944800b3e8f789cc250c
Anton Samarchyan <desecho@gmail.com>
2018-04-03 01:01:36 -0400

    Replace Javascript with JavaScript

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,15 +4,15 @@ export default class Header extends Component {
 render() {
     return (
       <div className="header">
-        <div className="row justify-content-md-center"> 
+        <div className="row justify-content-md-center">
           <h1>Artem Rosnovsky</h1>
         </div>
-          
-        <div className="row justify-content-md-center"> 
-          <h2>Full Stack Javascript Developer</h2>
+
+        <div className="row justify-content-md-center">
+          <h2>Full Stack JavaScript Developer</h2>
         </div>
-        
-        <div className="row justify-content-md-center"> 
+
+        <div className="row justify-content-md-center">
           <button type="button" className="btn btn-secondary"> <img src="../images/status.gif" alt="" className="status" /> Available for hire</button>
         </div>
       </div>
